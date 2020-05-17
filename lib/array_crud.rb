14c505ab5c86c95a3147d1457892aa60e4151d8e
Esther Kang <estherkang14@gmail.com>
2020-05-17 19:46:02 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,9 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array.push("scoups")
+  add_element_to_end_of_array = ["hello", "bye"]
+  add_element_to_end_of_array.push("arrays")
+  
 end
 
 def add_element_to_start_of_array(array, element)
